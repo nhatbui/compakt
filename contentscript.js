2877d4a2299601ee18cd98c5caa2c00db8018f4c
Nhat Bui <nhatbuiq@gmail.com>
2016-08-07 01:37:52 -0700

    Emoticon handling. Rework to support sentence compression also.

--- contentscript.js
+++ contentscript.js
@@ -4,12 +4,41 @@ order = [];
 size = 25;
 
 // Helper Methods
-function replaceMessageText(element, newMsg) {
-  element.text(newMsg);
-}
-
 function wrapInRepeatedWordClass(str) {
   return "<span class='repeated-word'>" + str + "</span>";
+}
+
+function compressStr(msg) {
+  var msgArray = msg.split(" ");
+  var prevString = msgArray[0];
+  var count = 1;
+  var newStr = "";
+  for (var i = 1; i < msgArray.length; i++) {
+    var word = msgArray[i];
+    if (word === "" || word === "\n") {
+      // Newlines and blank spaces are HTML we skipped.
+      // We won't need this if we handle them correctly.
+      continue;
+    }
+
+    if (prevString === word) {
+      count += 1;
+    } else {
+      if (count === 1) {
+        newStr += prevString + " ";
+      } else {
+        newStr += wrapInRepeatedWordClass(prevString + " x" + count) + " ";
+      }
+      count = 1;
+      prevString = word;
+    }
+  }
+  if (count === 1) {
+    newStr += prevString;
+  } else {
+    newStr += wrapInRepeatedWordClass(prevString + " x" + count);
+  }
+  return newStr;
 }
 
 // Simple compression. Adjacent, repeated strings are compressed to one instance
@@ -18,7 +47,7 @@ function compressMessage(msg) {
   var msgArray = msg.split(" ");
   var prevString = msgArray[0];
   var count = 1;
-  var newStr = "";
+  var newStr = [];
   for(var i = 1; i < msgArray.length; i++) {
     var word = msgArray[i];
     if(word === "" || word === "\n"){
@@ -29,38 +58,84 @@ function compressMessage(msg) {
     if(prevString === word) {
       count += 1;
     } else {
-      if(count === 1) {
-        newStr += prevString + " ";
-      } else {
-        newStr += wrapInRepeatedWordClass(prevString + " x" + count) + " ";
-      }
+      newStr.push({str: prevString, count: count});
       count = 1;
       prevString = word;
     }
   }
-  if(count === 1) {
-    newStr += prevString;
-  } else {
+  if(count > 1) {
     count += 1;
-    newStr += wrapInRepeatedWordClass(prevString + " x" + count);
   }
+  newStr.push({str: prevString, count: count});
 
   return newStr;
 }
 
-function removeRepeats(msg) {
+function compressMessageReadable(messageElement) {
+  var chatLine = messageElement.contents();
+
+  var prevElement = null;
+  var count = 0;
+
+  for (var i = 0; i < chatLine.length; i++) {
+    var type = chatLine[i].nodeType;
+    var ele = $(chatLine[i]);
+    if (type === 3) {
+      // Text node
+      if (chatLine[i].data.trim() === "") {
+        continue;
+      }
+
+      var newMsg = compressStr(chatLine[i].data.trim());
+      var temp = document.createElement('span');
+      temp.innerHTML = newMsg;
+      while (temp.firstChild) {
+          chatLine[i].parentNode.insertBefore(temp.firstChild, chatLine[i]);
+      }
+      chatLine[i].parentNode.removeChild(chatLine[i]);
+    } else if (type === 8) {
+      // Comment Node
+      continue;
+    } else if (type === 1) {
+      var ele = $(chatLine[i]);
+      var eleText = ele.text().trim();
+      // Node should be an emoticon
+      if (prevElement) {
+        if (prevElement === eleText ){
+          // This is a repeated element. Remove it.
+          ele.remove();
+          count += 1;
+        } else {
+          // The end of a run. (could be a run of length 1!)
+          if(count > 1) {
+            ele.append(" x" + count);
+            ele.addClass("repeated-word");
+          }
+          prevElement = eleText;
+          count = 1;
+        }
+      } else {
+        // This is the first emoticon we see. Set it and continue.
+        prevElement = eleText;
+        count = 1;
+      }
+    }
+  }
+}
+
+function removeRepeatedWords(msg) {
   var msgArray = msg.split(" ");
   var prevString = msgArray[0];
   var newStr = "";
   for(var i = 1; i < msgArray.length; i++) {
     var word = msgArray[i];
-    if(word === "" || word === "\n"){
+    if(word === "" || word === "\n") {
       // Newlines and blank spaces are HTML we skipped.
       // We won't need this if we handle them correctly.
       continue;
     }
     if(prevString != word) {
-      newStr += prevString + " ";
+      newStr += prevString;
       prevString = word;
     }
   }
@@ -69,8 +144,38 @@ function removeRepeats(msg) {
   return newStr;
 }
 
-function makeKeyFromChatMessage(msg) {
-  return removeRepeats(msg).toLowerCase();
+function makeKeyFromChatMessage(messageElement) {
+  var chatLine = messageElement.contents();
+
+  var prevElement = null;
+  var key = "";
+
+  for (var i = 0; i < chatLine.length; i++) {
+    var type = chatLine[i].nodeType;
+    var ele = $(chatLine[i]);
+    if (type === 3) {
+      // Text node
+      if (ele.text().trim() === "") {
+        continue;
+      }
+      var newMsg = removeRepeatedWords(ele.text().trim());
+      key += newMsg + " ";
+    } else if (type === 8) {
+      // Comment Node
+      continue;
+    } else if (type === 1) {
+      // Node should be an emoticon
+      if (prevElement) {
+        if (prevElement != ele.text()) {
+          key += prevElement + " ";
+          prevElement = ele.text();
+        }
+      } else {
+        prevElement = ele.text();
+      }
+    }
+  }
+  return key.toLowerCase();
 }
 
 // Attach listener that acts when a new chat message appears.
@@ -80,15 +185,13 @@ var chatObserver = new MutationObserver(function (mutations) {
     // Chat message should be the added node
     mutation.addedNodes.forEach(function (addedNode) {
       var chatMessage = $(addedNode);
+      if(!chatMessage.hasClass("message-line")) {
+        return;
+      }
       // Grab the actual span element with the message content
       var messageElement = chatMessage.children(".message");
 
-      // TODO: We lose emotes (e.g. kappa). Fix!
-      //var newMessage = compressMessage(messageElement.text());
-      //replaceMessageText(messageElement, "");
-      //messageElement.append(newMessage);
-
-      var key = makeKeyFromChatMessage(messageElement.text());
+      var key = makeKeyFromChatMessage(messageElement);
       if(key in dict) {
         // Update Cached Message
         var msgEle = $(dict[key].ele);
@@ -107,7 +210,7 @@ var chatObserver = new MutationObserver(function (mutations) {
 
         var dropdownMenu = msgEle.find(".compakt-dropdown-content");
         var userEle = chatMessage.children(".from");
-        dropdownMenu.append("<a href='#'>" + userEle.text() + "</a>");
+        dropdownMenu.append(userEle);
 
         // Don't show it, it's a repeat.
         chatMessage.hide();
@@ -115,9 +218,8 @@ var chatObserver = new MutationObserver(function (mutations) {
         // add unique message to buffer.
 
         // Sanitize the message.
-        var compressedMsg = compressMessage(messageElement.text());
-        replaceMessageText(messageElement, "");
-        messageElement.append(compressedMsg);
+        // Method does this "in-place" (edits existing DOM element).
+        compressMessageReadable(messageElement);
 
         // Update the ordered dictionary
         dict[key] = {ele: chatMessage, count: 1};
@@ -127,8 +229,9 @@ var chatObserver = new MutationObserver(function (mutations) {
         if(order.length > size) {
           // Add a checkmark to distinguish that the message has expired.
           var oldChatMessage = dict[order[0]].ele;
-          var oldMsg = oldChatMessage.children(".message");
-          oldMsg.append("&#10003;");
+          //var oldMsg = oldChatMessage.children(".count");
+          //oldMsg.append("&#10003;");
+          oldChatMessage.append("&#10003;");
 
           delete dict[order.shift()];
         }
