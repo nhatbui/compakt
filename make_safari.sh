1d7396c5e4840538e3026428dc986ffd86cfa94f
Nhat Bui <nhatbui@Nhats-MBP.fios-router.home>
2016-11-20 00:27:29 -0800

    Add xcode proj for Safari dev

---
+++ make_safari.sh
@@ -0,0 +1,3 @@
+cp contentscript.js safari/compakt/compakt_ext/script.js;
+cp css/compaktstyles.css safari/compakt/compakt_ext/;
+cp assets/icon_web.pdf safari/compakt/compakt_ext/ToolbarItemIcon.pdf
---
+++ safari/compakt/compakt_ext/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  compakt_ext
+//
+//  Created by Nhat Bui on 11/19/16.
+//  Copyright © 2016 Nhat Bui. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
