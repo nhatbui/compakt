e5af7af80445b3862a18b4898b5d34f6200c0a0b
Nhat Bui <nhatbui@Nhats-MBP.fios-router.home>
2016-12-18 23:06:36 -0800

    Scripts reflect new output dir

--- make_chrome.sh
+++ make_chrome.sh
@@ -1,11 +1,10 @@
 gulp --production --gulpfile gulpfile_chrome.js;
 # Copy global assets into chrome folder
-cp css/compaktstyles.css chrome/
-cp contentscript.js chrome/
-zip chrome/compakt \
-    chrome/contentscript.js \
-    chrome/assets/*.png \
-    chrome/checkIfTwitch.js \
-    LICENSE.txt \
-    chrome/manifest.json \
-    chrome/compaktstyles.css
+cp css/compaktstyles.css build/chrome/
+cp contentscript.js build/chrome/
+zip build/chrome/compakt \
+    build/chrome/contentscript.js \
+    build/chrome/assets/*.png \
+    build/chrome/checkIfTwitch.js \
+    build/chrome/manifest.json \
+    build/chrome/compaktstyles.css
--- make_safari.sh
+++ make_safari.sh
@@ -1,4 +1,4 @@
-gulp --type=production --gulpfile gulpfile_safari.js;
-cp contentscript.js safari/compakt/compakt_ext/script.js;
-cp css/compaktstyles.css safari/compakt/compakt_ext/;
-cp safari/assets/icon_web.pdf safari/compakt/compakt_ext/ToolbarItemIcon.pdf;
+gulp --production --gulpfile gulpfile_safari.js;
+cp contentscript.js build/safari/compakt/compakt_ext/script.js;
+cp css/compaktstyles.css build/safari/compakt/compakt_ext/;
+cp build/safari/assets/icon_web.pdf build/safari/compakt/compakt_ext/ToolbarItemIcon.pdf;
